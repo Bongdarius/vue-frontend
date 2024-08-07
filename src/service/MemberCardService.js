94b2217094c7ecac906c8ea78eb448862c0f9042
Bongdarius <okuo94@gnu.ac.kr>
2024-08-08 00:55:01 +0900

    리스트로 저장 삭제 추가

--- src/service/MemberCardService.js
+++ src/service/MemberCardService.js
@@ -14,6 +14,15 @@ export default class MemberCardService {
 				throw new Error(error.response.data.message);
 			});
 	}
+	insertList(memberCard) {
+		return axios.post("/memberCard/list", memberCard)
+			.then((res) => {
+				return res.data;
+			})
+			.catch((error) => {
+				throw new Error(error.response.data.message);
+			});
+	}
 	updateOne(memberCard) {
 		return axios.put(`/memberCard`, memberCard)
 			.then((res) => {
@@ -23,8 +32,26 @@ export default class MemberCardService {
 				throw new Error(error.response.data.message);
 			});
 	}
+	updateList(memberCard) {
+		return axios.put(`/memberCard/list`, memberCard)
+			.then((res) => {
+				return res.data;
+			})
+			.catch((error) => {
+				throw new Error(error.response.data.message);
+			});
+	}	
 	deleteOne(mcSeq) {
 		return axios.delete(`/memberCard/${mcSeq}`)
+			.then((res) => {
+				return res.data;
+			})
+			.catch((error) => {
+				throw new Error(error.response.data.message);
+			});
+	}	
+	deleteList(memberCard) {
+		return axios.put("/memberCard/deletelist", memberCard)
 			.then((res) => {
 				return res.data;
 			})
