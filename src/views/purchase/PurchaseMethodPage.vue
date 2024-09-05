f9107dccde13d368fc721fccf434f51579f724df
Bongdarius <okuo94@gnu.ac.kr>
2024-09-06 01:47:10 +0900

    그리드 유틸을 이용한 방식으로 수정개발 완료

--- src/views/purchase/PurchaseMethodPage.vue
+++ src/views/purchase/PurchaseMethodPage.vue
@@ -6,58 +6,60 @@
       <Button label="삭제" @click="deleteRows" class="flex-grow" />
       <Button label="저장" @click="save" class="flex-grow" />
     </div>
-    <grid
-      ref="gridRef"
-      :data="gridProps.data"
-      :columns="gridProps.columns"
-      :options="gridProps.options"
-      :theme="gridProps.myTheme"
-      @check="onCheck"
-      @uncheck="onUnCheck"
-      :rowHeaders="gridProps.rowHeaders"
-      :columnOptions="gridProps.columnOptions"
-    ></grid>
+  <div id="grid"></div>    
   </div>
 </template>
 <script>
-import 'tui-grid/dist/tui-grid.css';
-import Grid from '@/Grid.vue';
-import { onMounted, reactive, ref } from 'vue';
+// eslint-disable-next-line no-unused-vars
+import Grid from 'tui-grid'
+import { onMounted } from 'vue';
 import PurchaseMethodService from '@/service/PurchaseMethodService';
+import GridUtils from '@/utilities/GridUtils';
 
 export default {
-  components: {
-    grid: Grid,
-  },
   setup() {
     onMounted(async () => {
+      grid = GridUtils.createGrid({
+        el: document.getElementById("grid"),
+        columns: [
+          {
+            header: '일련번호',
+            name: 'pcmSeq',
+          },
+          {
+            header: '결제수단',
+            name: 'pcmNm',
+            editor: 'text',
+          },       
+        ],
+      });
+
       search();
     });
+
+    /**
+     * @type {Grid}
+     */
+    let grid = null;
+
     const search = () => {
       purchaseMethodService.selectList()
         .then((data) => {
-          // data.forEach((each, idx) => {
-          //   data[idx].text = each.pcmNm;
-          //   data[idx].value = each.pcmSeq;
-          // })
-
-          gridRef.value.invoke("resetData", data);
-          // gridProps.columns[2].editor.options.listItems = data;
-          // gridRef.value.invoke("setColumns", gridProps.columns);
+          grid.resetData(data);
         })
     }
     const appendRow = () => {
-      gridRef.value.invoke("appendRows", [{}]);
+      grid.appendRows([{}]);
     }
     const deleteRows = () => {
       /**
        * @type {Number[]}
        */
-      const checkedList = gridRef.value.invoke("getCheckedRowKeys");
-      gridRef.value.invoke("removeRows", checkedList);
+      const checkedList = grid.getCheckedRowKeys();
+      grid.removeRows(checkedList);
     }
     const save = () => {
-      const modifiedRows = gridRef.value.invoke("getModifiedRows");
+      const modifiedRows = grid.getModifiedRows();
       const createdRows = modifiedRows.createdRows;
       const updatedRows = modifiedRows.updatedRows;
       const deletedRows = modifiedRows.deletedRows;
@@ -77,75 +79,18 @@ export default {
       }
       if(deletedRows.length > 0) {
         deletedRows.forEach(each => {
-          purchaseMethodService.deleteOne(each.cardSeq)
+          purchaseMethodService.deleteOne(each.pcmSeq)
             .catch((e) => { alert(e.message); })
             .finally(() => { search(); })
         })
       }
     }
     const purchaseMethodService = new PurchaseMethodService();
-    const gridRef = ref(null);
-    const listItems = ref([]);
-    const gridProps = reactive({
-      rowHeaders: ['checkbox', 'rowNum'],
-      columnOptions: {
-        resizable: true,
-        frozenCount: 1,
-      },
-      scrollX: true,
-      scrollY: true,
-      bodyHeight: 200,
-      columns: [
-        {
-          header: '일련번호',
-          name: 'pcmSeq',
-        },
-        {
-          header: '결제수단',
-          name: 'pcmNm',
-          editor: 'text',
-        },       
-        // {
-        //   header: 'Type',
-        //   name: 'typeCode',
-        //   formatter: 'listItemText',
-        //   editor: {
-        //     type: 'select',
-        //     options: {
-        //       listItems: []
-        //     }
-        //   }
-        // },
-      ],
-      data: [],
-      myTheme: {
-        name: 'myTheme',
-        value: {
-          cell: {
-            normal: {
-              background: '#00ff00',
-              border: '#e0e0e0',
-            },
-            header: {
-              background: '#ff0000',
-              border: '#ffff00',
-            },
-            editable: {
-              background: '#fbfbfb',
-            },
-          },
-        },
-      },
-      options: {
-        rowHeaders: ['checkbox'],
-      },
-    });
     return {
-      gridProps, purchaseMethodService, gridRef, appendRow, save, search, deleteRows, listItems,
+      grid,
+      purchaseMethodService, 
+      appendRow, save, search, deleteRows, 
     }
   },
 };
 </script>
-<style>
-@import 'https://uicdn.toast.com/tui-grid/latest/tui-grid.css';
-</style>
